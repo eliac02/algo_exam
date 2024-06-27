bde77ab68e26ecbe2b9ece2b655559b1946cc32a
eliacortesi <eliacortesi278@gmail.com>
2024-06-27 19:32:42 +0200

    update

--- lung.go
+++ lung.go
@@ -3,6 +3,10 @@ package main
 
 import "fmt"
 
+// lung retrieve the lenght of the shortest path between two tiles
+//
+// @param p The system tiles-rules
+// @param x1 x2 y1 y2 The coordinates of the two tiles
 func lung(p piano, x1, y1, x2, y2 int) {
 	start := piastrella{x: x1, y: y1}
 	end := piastrella{x: x2, y: y2}
